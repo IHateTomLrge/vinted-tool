63c926736e21f11b42007e20ba197cddf8485196
tomlarge <large.tom.pro@gmail.com>
2021-09-25 16:26:20 +0200

    first commit

---
+++ helper/client.py
@@ -0,0 +1,27 @@
+from typing import Tuple
+import random
+import cloudscraper
+
+'''Init client class'''
+class Client(object):
+    
+    @property
+    def client(self) -> Tuple[cloudscraper.CloudScraper, str]:
+        platforms: list = [
+            'ios',
+            'android',
+            'windows',
+            'darwin',
+            'linux'
+        ]
+
+        session: cloudscraper.CloudScraper = cloudscraper.create_scraper(
+            browser={
+                'browser': 'chrome',
+                'mobile': True,
+                'platform': random.choice(platforms)
+            }
+        )
+        ua = session.headers['User-Agent']
+        return session, ua
+
---
+++ main.py
@@ -0,0 +1,29 @@
+from helper.tool import Tool
+
+'''
+Product, ex: Jordan 1, Nike, Adidas, etc.
+Maximum price, ex: 180 (for 180€)
+Discord webhook url to be used for sending products founds, ex: https://discordapp.com/api/webhooks/...
+Proxies, can be a list of proxies, a string, or None if you don't want to use proxy.
+The Proxy helper will automatically convert to usable proxy.
+proxies: str = ''
+proxies: list = []
+proxies = None
+
+status
+6 = new with label
+1 = new without label
+2 = Very good state
+3 = Good state
+4 = Medium state
+
+multiple status seperated by comma (6,1,2)
+'''
+
+proxies = None
+product: str = 'Air force 1 Off white'
+maxPrice: int = 200
+status: str = '6,1'
+webhook: str = 'https://discord.com/api/webhooks/850556437678784523/I2j8Lo25tiuQUcPbeX6J7IfJ2eV3YiGQrrVRjN0gaIgWdDn3PuXKCrcVY7PshyC9aVaz'
+
+Tool(product, maxPrice, webhook, proxies)
